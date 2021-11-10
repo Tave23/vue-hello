45e582e046c872cf143eca7a5cfcae6c2a0bea43
Davide Tavella <tavella01@gmail.com>
2021-11-10 16:02:52 +0100

    stampa immagine e stampa nome

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+const app = new Vue({
+   // qui si inserisce TUTTO il codice vue
+   el: '#app',
+   // in data inserisco tutte le variabili e/o costanti
+   data: {
+     nomeUtente: 'Davide',
+     imageRonaldo: 'img/Ronaldo.jpeg'
+   },
+
+   // dentro methods inserisco tutte le funzioni del caso
+   methods:{
+     
+      
+   }
+ 
+ 
+ 
+ 
+ 
+ 
+ })
